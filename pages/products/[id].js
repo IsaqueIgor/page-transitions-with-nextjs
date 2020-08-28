3fa457fd36ae96dc2ee717805b68146ecf0e3cc0
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-08-28 12:57:29 -0300

    feat: add motion transitions

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,52 +1,113 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import fetch from 'isomorphic-unfetch';
+import { BiArrowBack } from 'react-icons/bi';
 import Link from 'next/link';
 
-const Product = (props) => (
-  <div>
-    <div className='fullscreen'>
-      <div className='product'>
-        <div className='img'>
-          <img key={props.product.image} src={props.product.image} />
-        </div>
-        <div className='product-details'>
-          <div className='inner'>
-            <Link href='/'>
-              <div>
-                <a className='go-back'>Back to products</a>
-              </div>
-            </Link>
-            <div>
-              <span className='category'>Protein</span>
-            </div>
-            <h1>{props.product.name}</h1>
-            <p>{props.product.details}</p>
-            <div className='additonals'>
-              <span>Soy Free</span>
-              <span>Gluten Free</span>
-            </div>
-            <div className='qty-price'>
-              <div className='qty'>
-                <div className='minus'>-</div>
-                <div className='amount'>1</div>
-                <div className='add'>+</div>
-              </div>
-              <span className='price'>{props.product.price}</span>
-            </div>
-            <div className='btn-row'>
-              <button className='add-to-cart'> Add to cart</button>
-              <button className='subscribe'> Subscribe</button>
-            </div>
+let easing = [0.6, -0.05, 0.01, 0.99];
+
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: { duration: 0.6, ease: easing },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+    },
+  },
+};
+
+const Product = (props) => {
+  const [count, setCount] = useState(0);
+  const [price, setPrice] = useState(props.product.price);
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+    setPrice((prevPrice) => prevPrice + price);
+  };
+
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+    setPrice((prevPrice) => prevPrice - price);
+  };
+
+  return (
+    <motion.div initial='initial' animate='animate' exit={{ opacity: 0 }}>
+      <div className='fullscreen'>
+        <div className='product'>
+          <motion.div
+            className='img'
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+          >
+            <motion.img
+              key={props.product.image}
+              src={props.product.image}
+              animate={{ x: 0, opacity: 1 }}
+              initial={{ x: 200, opacity: 0 }}
+              exit={{ opacity: 0 }}
+              transition={{ delay: 0.2 }}
+            />
+          </motion.div>
+          <div className='product-details'>
+            <motion.div variants={stagger} className='inner'>
+              <Link href='/'>
+                <div>
+                  <a className='go-back'>
+                    <BiArrowBack size={23} />
+                  </a>
+                </div>
+              </Link>
+              <motion.div variants={fadeInUp}>
+                <span className='category'>Protein</span>
+              </motion.div>
+              <motion.h1 variants={fadeInUp}>{props.product.name}</motion.h1>
+              <motion.p variants={fadeInUp}>{props.product.details}</motion.p>
+              <motion.div variants={fadeInUp} className='additonals'>
+                <span>{props.product.protein}g Protein</span>
+                <span>Gluten Free</span>
+              </motion.div>
+              <motion.div variants={fadeInUp} className='qty-price'>
+                <div className='qty'>
+                  <div className='minus' onClick={handleDecrement}>
+                    -
+                  </div>
+                  <div className='amount'>{count}</div>
+                  <div className='add' onClick={handleIncrement}>
+                    +
+                  </div>
+                </div>
+                <span className='price'>$ {price}</span>
+              </motion.div>
+              <motion.div variants={fadeInUp} className='btn-row'>
+                <button className='add-to-cart'> Add to cart</button>
+                <button className='subscribe'> Subscribe</button>
+              </motion.div>
+            </motion.div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    </motion.div>
+  );
+};
 
 Product.getInitialProps = async function (context) {
   const { id } = context.query;
   const res = await fetch(
-    `http://my-json-server.typicode.com/wrongakram/demo/products/${id}`
+    `https://my-json-server.typicode.com/isaqueigor/page-transitions-with-nextjs/products/${id}`
   );
   const product = await res.json();
   return { product };
